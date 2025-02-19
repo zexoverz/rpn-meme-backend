c1efba9f4e222e7e0afd19213b8295187f6c6740
zexoverz <faisalfirdani01@gmail.com>
2025-02-19 17:40:52 +0700

    finishing all post validation

--- src/validations/post.validation.js
+++ src/validations/post.validation.js
@@ -1,23 +1,70 @@
 const Joi = require('joi');
 
 const createPost = {
-    body: Joi.object().keys({
-      caption: Joi.string(),
-      imageUrl: Joi.string(),
-      imageId: Joi.string(),
-      location: Joi.string(),
-      tags: Joi.array()
-    }),
+  body: Joi.object().keys({
+    caption: Joi.string(),
+    imageUrl: Joi.string(),
+    imageId: Joi.string(),
+    location: Joi.string(),
+    tags: Joi.array()
+  }),
 };
 
 const getPosts = {
-    query: Joi.object().keys({
-        page: Joi.number().integer(),
-        limit: Joi.number().integer(),
-    }),
+  query: Joi.object().keys({
+    page: Joi.number().integer(),
+    limit: Joi.number().integer(),
+  }),
+};
+
+const updatePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().required(),
+  }),
+  body: Joi.object().keys({
+    caption: Joi.string(),
+    location: Joi.string(),
+    tags: Joi.array()
+  }),
+};
+
+const deletePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().required(),
+  }),
+};
+
+const likePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().required(),
+  }),
+};
+
+const unlikePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().required(),
+  }),
+};
+
+const savePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().required(),
+  }),
+};
+
+const unsavePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().required(),
+  }),
 };
 
 module.exports = {
-    getPosts,
-    createPost
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  unlikePost,
+  savePost,
+  unsavePost
 };
