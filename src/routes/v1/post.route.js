0f7799166fa46285c069fd71bb8bec7c9a729158
zexoverz <faisalfirdani01@gmail.com>
2025-02-19 17:41:53 +0700

    finishing all post route

--- src/routes/v1/post.route.js
+++ src/routes/v1/post.route.js
@@ -7,8 +7,22 @@ const router = express.Router();
 
 router
   .route('/')
-  .get( validate(postValidation.getPosts), postController.getPosts)
-  .post(auth(), validate(postValidation.createPost), postController.createPost)
+  .get(validate(postValidation.getPosts), postController.getPosts)
+  .post(auth(), validate(postValidation.createPost), postController.createPost);
 
+router
+  .route('/:postId')
+  .patch(auth(), validate(postValidation.updatePost), postController.updatePost)
+  .delete(auth(), validate(postValidation.deletePost), postController.deletePost);
+
+router
+  .route('/:postId/like')
+  .post(auth(), validate(postValidation.likePost), postController.likePost)
+  .delete(auth(), validate(postValidation.unlikePost), postController.unlikePost);
+
+router
+  .route('/:postId/save')
+  .post(auth(), validate(postValidation.savePost), postController.savePost)
+  .delete(auth(), validate(postValidation.unsavePost), postController.unsavePost);
 
 module.exports = router;
