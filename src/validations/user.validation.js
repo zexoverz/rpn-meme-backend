6194465beab62adbdd0912e76404ca7d5f4caa23
zexoverz <faisalfirdani01@gmail.com>
2025-03-15 14:22:55 +0700

    remove validation

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -24,9 +24,6 @@ const getUser = {
 };
 
 const updateUser = {
-  params: Joi.object().keys({
-    userId: Joi.required().custom(objectId),
-  }),
   body: Joi.object()
     .keys({
       email: Joi.string().email(),
